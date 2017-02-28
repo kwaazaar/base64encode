274c64d687f2da0ab7991d28cf0f5df6fb0e762b
robertka <robertka@infosupport.com>
2017-02-28 10:49:18 +0100

    First version

--- src/Base64Encode/Program.cs
+++ src/Base64Encode/Program.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Base64Encode
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("base64encode <filename> [--oneline]");
+                return 1;
+            }
+
+            try
+            {
+                var base64string = Convert.ToBase64String(File.ReadAllBytes(args[0]));
+                if (args.Length > 1 && args[1].Equals("--oneline", StringComparison.OrdinalIgnoreCase))
+                    base64string = base64string.Replace("\n", "");
+                Console.WriteLine(base64string);
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return 2;
+            }
         }
     }
 }
